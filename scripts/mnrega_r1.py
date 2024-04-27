be0be45d8f8ec1eda57a07cb24f0219efecf2bc8
Suriyan Laohaprapanon <suriyant@gmail.com>
2024-04-27 03:02:49 +0200

    update R1 script to make it able to scrape data since year 2009

--- scripts/mnrega_r1.py
+++ scripts/mnrega_r1.py
@@ -8,6 +8,41 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+states = {'01': 'ANDAMAN AND NICOBAR',
+'02': 'ANDHRA PRADESH',
+'03': 'ARUNACHAL PRADESH',
+'04': 'ASSAM',
+'05': 'BIHAR',
+'33': 'CHHATTISGARH',
+'07': 'DN HAVELI AND DD',
+'10': 'GOA',
+'11': 'GUJARAT',
+'12': 'HARYANA',
+'13': 'HIMACHAL PRADESH',
+'14': 'JAMMU AND KASHMIR',
+'34': 'JHARKHAND',
+'15': 'KARNATAKA',
+'16': 'KERALA',
+'37': 'LADAKH',
+'19': 'LAKSHADWEEP',
+'17': 'MADHYA PRADESH',
+'18': 'MAHARASHTRA',
+'20': 'MANIPUR',
+'21': 'MEGHALAYA',
+'22': 'MIZORAM',
+'23': 'NAGALAND',
+'24': 'ODISHA',
+'25': 'PUDUCHERRY',
+'26': 'PUNJAB',
+'27': 'RAJASTHAN',
+'28': 'SIKKIM',
+'29': 'TAMIL NADU',
+'36': 'TELANGANA',
+'30': 'TRIPURA',
+'31': 'UTTAR PRADESH',
+'35': 'UTTARAKHAND',
+'32': 'WEST BENGAL'
+}
 
 def clean_and_save_csv(df, fn):
     bfn = os.path.basename(fn)
@@ -65,79 +100,62 @@ if __name__ == '__main__':
 
     s = requests.Session()
 
-    r1 = s.get(f'https://nreganarep.nic.in/netnrega/app_issue.aspx?lflag=eng&fin_year={year}-{year + 1}&source=national&labels=labels&Digest=HNrisV4bhHnb7Gve3mAKYQ', headers=headers)
-    if r1.status_code != 200:
-        print(f'Error to get start URL with status code: {r1.status_code}')
-        sys.exit(-1)
+    for statecode in states:
+        state = states[statecode]
+        r1 = s.get(f'https://nreganarep.nic.in/netnrega/app_issue.aspx?page=s&lflag=eng&state_name={state}&state_code={statecode}&fin_year={year}-{year + 1}&source=national&Digest=c0WQRf6oknNlNveH/sGS7A', headers=headers)
+        if r1.status_code != 200:
+            print(f'Error to get start URL with status code: {r1.status_code}')
+            sys.exit(-1)
 
-    dfs = pd.read_html(r1.content, encoding='utf-8')
-    df = dfs[6]
-    clean_and_save_csv(df, f'{year}-csv/r1_state.csv')
-
-    soup1 = BeautifulSoup(r1.content, 'lxml')
-    states = []
-    for l in soup1.find_all('a', {'href': re.compile('app_issue\.aspx.*')}):
-        print(l.text)
-        states.append((l.text, l['href']))
-    print(f'Number of state: {len(states)}')
-
-    for st in states:
-        state = st[0].strip()
         print('- State:', state)
-
-        url = 'https://nreganarep.nic.in/netnrega/' + st[1]
-        r2 = s.get(url, headers=headers)
-        if r2.status_code == 200:
-            try:
-                dfs = pd.read_html(r2.content, encoding='utf-8')
-                df = dfs[6]
-                df['state'] = state
-                clean_and_save_csv(df, f'{year}-csv/r1_district_{state}.csv')
-                soup2 = BeautifulSoup(r2.content, 'lxml')
-                districts = []
-                for l in soup2.find_all('a', {'href': re.compile('app_issue\.aspx.*')}):
-                    districts.append((l.text, l['href']))
-                for dt in districts:
-                    district = dt[0].strip()
-                    print('  - District:', district)
-                    url = 'https://nreganarep.nic.in/netnrega/' + dt[1]
-                    r3 = s.get(url, headers=headers)
-                    if r3.status_code == 200:
-                        try:
-                            dfs = pd.read_html(r3.content, encoding='utf-8')
-                            df = dfs[6]
-                            df['state'] = state
-                            df['district'] = district
-                            clean_and_save_csv(df, f'{year}-csv/r1_block_{district}+{state}.csv')
-                            soup3 = BeautifulSoup(r3.content, 'lxml')
-                            blocks = []
-                            for l in soup3.find_all('a', {'href': re.compile('app_issue\.aspx.*')}):
-                                blocks.append((l.text, l['href']))
-                            for bl in blocks:
-                                block = bl[0].strip()
-                                print('    - Block:', block)
-                                url = 'https://nreganarep.nic.in/netnrega/' + bl[1]
-                                r4 = s.get(url, headers=headers)
-                                if r4.status_code == 200:
-                                    try:
-                                        dfs = pd.read_html(r4.content, encoding='utf-8')
-                                        df = dfs[6]
-                                        df['state'] = state
-                                        df['district'] = district
-                                        df['block'] = block
-                                        clean_and_save_csv(df, f'{year}-csv/r1_panchayat_{block}+{district}+{state}.csv')
-                                    except Exception as e:
-                                        print('ERROR', e)
-                                else:
-                                    print('Block ERROR', r4.status_code, block)
-                                #break
-                        except Exception as e:
-                            print('ERROR:', e)
-                    else:
-                        print('District ERROR', r3.status_code, district)
-                    #break
-            except Exception as e:
-                print('ERROR:', e)
-        else:
-            print('State ERROR', r2.status_code, state)
+        try:
+            dfs = pd.read_html(r1.content, encoding='utf-8')
+            df = dfs[6]
+            df['state'] = state
+            clean_and_save_csv(df, f'{year}-csv/r1_district_{state}.csv')
+            soup2 = BeautifulSoup(r1.content, 'lxml')
+            districts = []
+            for l in soup2.find_all('a', {'href': re.compile('app_issue\.aspx.*')}):
+                districts.append((l.text, l['href']))
+            for dt in districts:
+                district = dt[0].strip()
+                print('  - District:', district)
+                url = 'https://nreganarep.nic.in/netnrega/' + dt[1]
+                r3 = s.get(url, headers=headers)
+                if r3.status_code == 200:
+                    try:
+                        dfs = pd.read_html(r3.content, encoding='utf-8')
+                        df = dfs[6]
+                        df['state'] = state
+                        df['district'] = district
+                        clean_and_save_csv(df, f'{year}-csv/r1_block_{district}+{state}.csv')
+                        soup3 = BeautifulSoup(r3.content, 'lxml')
+                        blocks = []
+                        for l in soup3.find_all('a', {'href': re.compile('app_issue\.aspx.*')}):
+                            blocks.append((l.text, l['href']))
+                        for bl in blocks:
+                            block = bl[0].strip()
+                            print('    - Block:', block)
+                            url = 'https://nreganarep.nic.in/netnrega/' + bl[1]
+                            r4 = s.get(url, headers=headers)
+                            if r4.status_code == 200:
+                                try:
+                                    dfs = pd.read_html(r4.content, encoding='utf-8')
+                                    df = dfs[6]
+                                    df['state'] = state
+                                    df['district'] = district
+                                    df['block'] = block
+                                    clean_and_save_csv(df, f'{year}-csv/r1_panchayat_{block}+{district}+{state}.csv')
+                                except Exception as e:
+                                    print('ERROR', e)
+                            else:
+                                print('Block ERROR', r4.status_code, block)
+                            #break
+                    except Exception as e:
+                        print('ERROR:', e)
+                else:
+                    print('District ERROR', r3.status_code, district)
+                #break
+        except Exception as e:
+            print('ERROR:', e)
         #break
